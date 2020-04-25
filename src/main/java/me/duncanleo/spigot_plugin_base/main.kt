005ae08a8c84b0b04a56c3973dbe87526a60f70a
Duncan Leo <hello@duncanleo.me>
2020-04-25 11:11:57 +0800

    chore: add IntelliJ config

--- src/main/java/me/duncanleo/spigot_plugin_base/main.kt
+++ src/main/java/me/duncanleo/spigot_plugin_base/main.kt
@@ -3,7 +3,6 @@ package me.duncanleo.spigot_plugin_base
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.event.Listener
 
-class App : JavaPlugin() {
 class App : JavaPlugin(), Listener {
   override fun onEnable() {
     logger.info("Hello there!")
